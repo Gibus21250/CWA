30245bd705cc86918a6df48afac2bb54c538eb63
Nathan J <jacquinet.nathan@gmail.com>
2023-12-07 13:19:17 +0100

    fusion page + adaptation js native -> ts style

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
+export class AccueilComponent implements OnInit{
+
+  dateActuelle: string | undefined;
+
+  ngOnInit(): void {
+    //Première initialisation
+    this.updateDate();
+
+    //On s'abonne à une clock de 1s, pour executer le code actualisant l'horloge dans l'affichage
+    interval(1000).subscribe(() => {
+      this.updateDate();
+    })
+  }
+
+  updateDate() {
+    let date = new Date();
+    this.dateActuelle = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' • ' + date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+  }
 
 }
