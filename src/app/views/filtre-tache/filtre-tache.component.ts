0fb26871283ef6570fba3bc50c0922a31d9d2b3f
Nathan J <jacquinet.nathan@gmail.com>
2023-12-08 17:08:26 +0100

    ajout dynamique des filtre, etat + theme

--- src/app/views/filtre-tache/filtre-tache.component.ts
+++ src/app/views/filtre-tache/filtre-tache.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FiltreInfos, Filtre } from "src/app/shared/enums/filtre";
+import { EtatInfos, Etat } from "src/app/shared/enums/etat";
+import { ThemeInfos, Theme } from "src/app/shared/enums/theme";
 
 @Component({
   selector: 'app-filtre-tache',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./filtre-tache.component.css']
 })
 export class FiltreTacheComponent {
+
+  //Données dynamique pour la liste de filtre
+  filtres = Object.values(Filtre).filter(value => typeof value === 'number');
+  FiltreInfos = FiltreInfos;
+
+  etats = Object.values(Etat).filter(value => typeof value === 'number')
+  EtatInfos = EtatInfos;
+
+  themes = Object.values(Theme).filter(value => typeof value === 'number')
+  ThemesInfo = ThemeInfos;
+
+  onTrierChange(event: any) : void {
+    let trie = event.target.value;
+    
+    switch (trie) {
+      case Filtre.NONE:
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  onFiltrerChange(event: any) : void {
+    
+  }
+
+  onThemeChange(event: any) : void {
+    
+  }
+
+  isFiltre(value: any): value is Filtre {
+    return typeof value === 'number';
+  }
+
+  isEtat(value: any): value is Etat {
+    return typeof value === 'number';
+  }
+
+  isTheme(value: any): value is Theme {
+    return typeof value === 'number';
+  }
 
 }
