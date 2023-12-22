e515e8bf7d2dd35f6a52c4aa5937c1c29636eefb
Iris10763 <94052867+Iris10763@users.noreply.github.com>
2023-12-22 19:16:11 +0100

    Boutons priorités remplis

--- src/app/views/creation-tache/creation-tache.component.ts
+++ src/app/views/creation-tache/creation-tache.component.ts
@@ -11,17 +11,48 @@ export class CreationTacheComponent {
 
   constructor(private router: Router) {}
 
-  dateControl = new FormControl(); //FormControl pour suivre la date
-  selectedOption: string = 'option1'; // État initial pour
+  nomTache: string = ''; // Nom de la tâche
+  dateControl = new FormControl(); //FormControl pour la date
+  description: string = ''; // Description de la tâche
+  selectedButton: number | null = null; // État initial pour la sélection de priorité
+  selectedEtat: string = 'aFaire'; // État initial pour la sélection d'état
+
+  // Priorités
+  selectButton(buttonNumber: number): void {
+    this.selectedButton = buttonNumber;
+  }
+
+  // Etats
+  selectEtat(etat: string): void {
+    this.selectedEtat = etat;
+  }
 
   // Fermer la popup
   closePopup() {
     this.router.navigate(['/']); 
   }
 
+  verificateur() {
+    if (this.nomTache.length > 0 && this.description.length > 0 && this.selectedButton !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   //Valider la création de tâche
   submit() {
+    const nomTache = this.nomTache; // Récupérer le nom de la tâche
     const selectedDate = this.dateControl.value; // Récupérer la date
+    const description = this.description; // Récupérer la description
+    const selectedButton = this.selectedButton; // Récupérer la priorité
+    const selectedEtat = this.selectedEtat; // Récupérer l'Etat
+    
+
+
+    //vérifier les données
+    //faire une instance de tâche
+
 
     //récupérer les valeurs des champs
     // Logique pour soumettre la tache
