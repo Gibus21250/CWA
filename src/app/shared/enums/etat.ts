2c2cb238eb1198a6bc9f4f5ac77c964a86ff8a59
Nathan J <jacquinet.nathan@gmail.com>
2023-12-22 21:57:43 +0100

    suppression etat

--- src/app/shared/enums/etat.ts
+++ src/app/shared/enums/etat.ts
@@ -1,12 +1,10 @@
 export enum Etat {
-    NONE,
     EN_ATTENTE,
     EN_COURS,
     TERMINE
 }
 
 export const EtatInfos: {[key in Etat]: string} = {
-    [Etat.NONE]: '--Aucun filtre--',
     [Etat.EN_ATTENTE]: 'En attente',
     [Etat.EN_COURS]: 'En cours',
     [Etat.TERMINE]: 'Terminée',
