59e61baef94b34b99130e241f6fa29b791974426
Nathan J <jacquinet.nathan@gmail.com>
2023-12-22 21:57:21 +0100

    ajout post sur le serv

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 25565;
 
 const fs = require('fs');
 app.use(cors());
 app.use(express.json());
 
-// Définir une route pour obtenir les tâches en fonction du thème
+//Charger
 app.get('/datas/:theme', (req, res) => {
   const theme = req.params.theme;
   const filePath = path.join(__dirname, 'src', 'datas', `${theme}`);
 
-  // Lire le fichier JSON correspondant au thème
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
@@ -23,6 +22,16 @@ app.get('/datas/:theme', (req, res) => {
     const tasks = JSON.parse(data);
     res.json(tasks);
   });
+});
+
+//Sauvegarder
+app.post('/datas/:theme', (req, res) => {
+  console.log("POST reçu");
+  const theme = req.params.theme;
+  const filePath = path.join(__dirname, 'src', 'datas', `${theme}`);
+
+  fs.writeFileSync(filePath, JSON.stringify(req.body), 'utf8');
+  res.status(200).json({ message: 'Données mises à jour avec succès.' });
 });
 
 app.listen(port, () => {
