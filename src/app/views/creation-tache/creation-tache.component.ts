01d3c1077699285cc3a4331979dd15d7be055684
Iris10763 <94052867+Iris10763@users.noreply.github.com>
2023-12-22 20:05:31 +0100

    Debut submit() création
    A reprendre avec nathan

--- src/app/views/creation-tache/creation-tache.component.ts
+++ src/app/views/creation-tache/creation-tache.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
+import { Tache } from 'src/app/shared/models/tache';
 
 @Component({
   selector: 'app-creation-tache',
@@ -9,13 +10,17 @@ import { FormControl } from '@angular/forms';
 })
 export class CreationTacheComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
+  //tache: Tache = new Tache('', new Date(), new Date(), '', 0, 'aFaire'); // Tâche à créer
   nomTache: string = ''; // Nom de la tâche
-  dateControl = new FormControl(); //FormControl pour la date
+  selectedDate = new FormControl(); //FormControl pour la date
   description: string = ''; // Description de la tâche
   selectedButton: number | null = null; // État initial pour la sélection de priorité
-  selectedEtat: string = 'aFaire'; // État initial pour la sélection d'état
+  selectedEtat: number = 0; // État initial pour la sélection d'état en radio bouton
+  etats: string[] = ['A faire', 'En cours', 'Terminée']; // Liste des états
+  etatTache: string = 'A faire'; // État de la tâche
 
   // Priorités
   selectButton(buttonNumber: number): void {
@@ -23,8 +28,8 @@ export class CreationTacheComponent {
   }
 
   // Etats
-  selectEtat(etat: string): void {
-    this.selectedEtat = etat;
+  selectEtat(selectedEtatProv: number): void {
+    this.etatTache = this.etats[selectedEtatProv];
   }
 
   // Fermer la popup
@@ -32,30 +37,47 @@ export class CreationTacheComponent {
     this.router.navigate(['/']); 
   }
 
-  verificateur() {
-    if (this.nomTache.length > 0 && this.description.length > 0 && this.selectedButton !== null) {
-      return true;
-    } else {
+  verificateur(nomTache: string, dateControl: FormControl, description: string, selectedButton: number | null) {
+    //Vérifier que les champs ne sont pas vides
+    if (nomTache == '' || dateControl == null || description == '' || selectedButton == null) {
       return false;
+    }
+    else {
+      //vérifier que la date ne soit pas dépassée
+      const date = new Date();
+      const dateNow = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+      const dateSelected = dateControl.value;
+      if (dateSelected < dateNow) {
+        return false;
+      }
+      return true;
     }
   }
 
   //Valider la création de tâche
   submit() {
-    const nomTache = this.nomTache; // Récupérer le nom de la tâche
-    const selectedDate = this.dateControl.value; // Récupérer la date
-    const description = this.description; // Récupérer la description
-    const selectedButton = this.selectedButton; // Récupérer la priorité
-    const selectedEtat = this.selectedEtat; // Récupérer l'Etat
+    //TESTS
+    alert('Nom de la tâche : ' + this.nomTache+' /n Date : ' + this.selectedDate+'/nDescription : ' + this.description+'/nPriorité : ' + this.selectedButton+'/nEtat : ' + this.etatTache);
+
     
+    //Vérifier les données
+    if(this.verificateur(this.nomTache, this.selectedDate, this.description, this.selectedButton)) {
+      //Si les données sont valides, on instancie la tâche
+      //this.creationTache(this.nomTache, this.selectedDate, this.description, this.selectedButton, this.etatTache);
 
+      //Ajouter la tâche dans la liste de tâches
+      //this.addTache(this.tache);
 
-    //vérifier les données
-    //faire une instance de tâche
+      //On affiche un message de confirmation
+      alert('Tâche créée avec succès !');
 
-
-    //récupérer les valeurs des champs
-    // Logique pour soumettre la tache
+      //On ferme la popup
+      this.closePopup();
+    }
+    else {
+      //Sinon, on affiche un message d'erreur
+      alert('Veuillez remplir tous les champs.');
+    }
   }
 
 }
